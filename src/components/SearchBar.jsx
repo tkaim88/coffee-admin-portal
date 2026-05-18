b38e904ca5c64f6ca8896966f8ed9099c28b5220
tkaim88 <tkomoruh@gmail.com>
2026-05-18 12:44:19 +0300

    changes to the homepage and search feature

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -19,6 +19,7 @@ function SearchBar({ search, setSearch }) {
 
       className="search-input"
     />
+
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
 // Home component
 // Landing page of the application
 
+import { useState } from "react";
+import SearchBar from "../components/SearchBar";
+
 function Home() {
+
+  // Stores the search input value entered by the user
+  const [search, setSearch] = useState("");
 
   return (
 
@@ -13,13 +19,25 @@ function Home() {
       {/* Description paragraph */}
       <p>
 
-        Your shop managing solution for all things coffee! This portal allows you
-        to manage products, update prices, search inventory,
+        Your shop managing solution for all things coffee!
+        This portal allows you to manage products,
+        update prices, search inventory,
         and maintain an organized e-commerce system.
 
       </p>
+
+      {/* Search section positioned below welcome text */}
+      {/* Props are passed to SearchBar so state can be controlled here */}
+
+      <SearchBar
+        search={search}
+        setSearch={setSearch}
+      />
+
     </div>
+
   );
+
 }
 
 export default Home;
