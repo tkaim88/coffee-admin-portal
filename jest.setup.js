d4f3cfe836ce33a37ba3e9f6f2008cd1237d2160
tkaim88 <tkomoruh@gmail.com>
2026-05-18 10:01:16 +0300

    fix global not defined and removed the tests folder

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom";
 import { TextEncoder, TextDecoder } from "util";
 
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
+globalThis.TextEncoder = TextEncoder;
+globalThis.TextDecoder = TextDecoder;
